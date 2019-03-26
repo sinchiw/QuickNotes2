ff557002cf20e3ef7e8914f2cb0baca075908f64
Wilmer sinchi <wilmersinchi@gmail.com>
2019-03-26 12:06:47 -0400

    delete functionality

---
+++ NoteMe2/View/CollectionViewLayout.swift
@@ -0,0 +1,13 @@
+//
+//  CollectionViewLayout.swift
+//  NoteMe2
+//
+//  Created by Wilmer sinchi on 3/21/19.
+//  Copyright © 2019 Wilmer sinchi. All rights reserved.
+//
+
+import UIKit
+
+class CollectionViewLayout: UICollectionViewLayout {
+
+}
---
+++ NoteMe2/View/MainCollectionViewLayout.swift
@@ -0,0 +1,22 @@
+//
+//  UICollectionViewFlowLayoutVC.swift
+//  NoteMe2
+//
+//  Created by Wilmer sinchi on 3/21/19.
+//  Copyright © 2019 Wilmer sinchi. All rights reserved.
+//
+
+import UIKit
+
+
+protocol PinterestLayoutDelegate: class {
+    func collectionView(_ collectionView:UICollectionView, heightForEachCell indexPath:IndexPath) -> CGFloat
+}
+
+class MainCollectionViewLayout: UICollectionViewLayout {
+    
+    weak var delegate:
+    
+
+}
+
